f7631d2948212f37924b38f0e9c29892ee59fc05
flyguy62n <rjbartels@outlook.com>
2021-02-15 14:15:27 -0500

    Add USB troubleshooting scripts

--- scripts/usb-dev-id.sh
+++ scripts/usb-dev-id.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+read -p "Make sure the device is NOT plugged in and press [Enter] to continue..."
+
 echo "Taking snapshot of /dev directory"
 ls /dev > /tmp/before.txt
 
@@ -7,9 +9,9 @@ read -p "Insert device and press [Enter] to continue..."
 
 ls /dev > /tmp/after.txt
 
-echo "Displaying differences"
+echo -e "BEFORE\t\t\t\t\t\t\t\tAFTER"
 diff -y --suppress-common-lines /tmp/before.txt /tmp/after.txt
 
-echo "Removing files"
+echo "Removing /tmp files"
 rm /tmp/before.txt /tmp/after.txt
 
